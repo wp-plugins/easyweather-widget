499dda975e8412cc5e492f1033b5f1d5b94f9130
myselfko <myselfko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-08 10:42:11 +0000

    Initial release
    git-svn-id: https://plugins.svn.wordpress.org/easyweather-widget/trunk@635764 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ weather-language.php
@@ -0,0 +1,51 @@
+<?php
+$weather_language = array();
+$weather_language['en'] = array(
+	"description" => "English",
+	"refreshed" => "Updated at",
+	"error" => "No cities match your search query.",
+	"Monday" => "Mon",
+	"Tuesday" => "Tue",
+	"Wednesday" => "Wed",
+	"Thursday" => "Thu",
+	"Friday" => "Fri",
+	"Saturday" => "Sat",
+	"Sunday" => "Sun"
+);
+$weather_language['si'] = array(
+	"description" => "Slovenščina",
+	"refreshed" => "Osveženo ob",
+	"error" => "Ne najdem mesta s tem ID.",
+	"Monday" => "Pon",
+	"Tuesday" => "Tor",
+	"Wednesday" => "Sre",
+	"Thursday" => "Čet",
+	"Friday" => "Pet",
+	"Saturday" => "Sob",
+	"Sunday" => "Ned"
+);
+$weather_language['de'] = array(
+	"description" => "Deutsch",
+	"refreshed" => "Aktualisiert am",
+	"error" => "Keine Städten entsprechen Ihrer Suchanfrage.",
+	"Monday" => "Mon",
+	"Tuesday" => "Die",
+	"Wednesday" => "Mit",
+	"Thursday" => "Don",
+	"Friday" => "Fre",
+	"Saturday" => "Sam",
+	"Sunday" => "Son"
+);
+$weather_language['it'] = array(
+	"description" => "Italiano",
+	"refreshed" => "Aggiornato al",
+	"error" => "Qui non posso trovare ID.",
+	"Monday" => "Lun",
+	"Tuesday" => "Mar",
+	"Wednesday" => "Mer",
+	"Thursday" => "Gio",
+	"Friday" => "Ven",
+	"Saturday" => "Sab",
+	"Sunday" => "Dom"
+);
+?>
